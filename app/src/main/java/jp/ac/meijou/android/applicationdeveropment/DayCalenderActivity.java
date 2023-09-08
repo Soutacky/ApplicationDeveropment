c4d0df356c4fa5d8141cfe1606d47d1a08e2985a
Yuta Mitsukawa <221205163@ccmailg.meijo-u.ac.jp>
2023-09-08 11:56:08 +0900

    カレンダーの日数の表示のアクティビティを追加

---
+++ app/src/main/java/jp/ac/meijou/android/applicationdeveropment/DayCalenderActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.meijou.android.applicationdeveropment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DayCalenderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_calender);
+    }
+}
