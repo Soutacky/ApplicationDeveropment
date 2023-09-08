81d73f053e6e76ddb1b6b082ecffde0a53662171
Takumi Hirabayashi <221205139@ccmailg.meijo-u.ac.jp>
2023-09-08 13:36:24 +0900

    ボタンで移動できるようにした

--- app/src/main/java/jp/ac/meijou/android/applicationdeveropment/FirstSettingActivity.java
+++ app/src/main/java/jp/ac/meijou/android/applicationdeveropment/FirstSettingActivity.java
@@ -2,13 +2,25 @@ package jp.ac.meijou.android.applicationdeveropment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import jp.ac.meijou.android.applicationdeveropment.databinding.ActivityFirstSettingBinding;
+
 public class FirstSettingActivity extends AppCompatActivity {
+    private ActivityFirstSettingBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first_setting);
+        binding = ActivityFirstSettingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.okButton.setOnClickListener(view -> {
+            var intent = new Intent(this, FirstMonsterSettingActivity.class);
+            intent.putExtra("text", binding.firstNameEdit.getText().toString());
+            startActivity(intent);
+            finish();
+        });
     }
 }
--- app/src/main/java/jp/ac/meijou/android/applicationdeveropment/SettingActivity.java
+++ app/src/main/java/jp/ac/meijou/android/applicationdeveropment/SettingActivity.java
@@ -2,17 +2,30 @@ package jp.ac.meijou.android.applicationdeveropment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-//import jp.ac.meijou.android.applicationdeveropment.databinding.ActivitySettingBinding;
+import jp.ac.meijou.android.applicationdeveropment.databinding.ActivitySettingBinding;
 
 public class SettingActivity extends AppCompatActivity {
-//    private ActivitySettingBinding binding;
+    private ActivitySettingBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        binding = ActivitySettingBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
+        binding = ActivitySettingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.okButton.setOnClickListener(view -> {
+            var intent = new Intent(this, TopActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        binding.cancelButton.setOnClickListener(view -> {
+            var intent = new Intent(this, TopActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
