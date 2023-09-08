c75cfd53b2370d2b1e59813c464c1893a00229eb
Takumi Hirabayashi <221205139@ccmailg.meijo-u.ac.jp>
2023-09-08 10:51:42 +0900

    設定画面の追加

---
+++ app/src/main/java/jp/ac/meijou/android/applicationdeveropment/SettingActivity.java
@@ -0,0 +1,18 @@
+package jp.ac.meijou.android.applicationdeveropment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//import jp.ac.meijou.android.applicationdeveropment.databinding.ActivitySettingBinding;
+
+public class SettingActivity extends AppCompatActivity {
+//    private ActivitySettingBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        binding = ActivitySettingBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+    }
+}
