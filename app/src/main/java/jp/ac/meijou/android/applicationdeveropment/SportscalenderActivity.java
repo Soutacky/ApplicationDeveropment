49edca005713cf3baf735df8ba5ab8248b072ffc
Souta Takiguchi <221205099@ccmailg.meijo-u.ac.jp>
2023-09-08 13:38:16 +0900

    カレンダー画面を2つ追加

---
+++ app/src/main/java/jp/ac/meijou/android/applicationdeveropment/SportscalenderActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.meijou.android.applicationdeveropment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SportscalenderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sportscalender);
+    }
+}
